18ce7e67776c4922f144dda70062bb69f6a5e279
haruna <w10776e8w@yahoo.co.jp>
2025-10-30 04:45:00 +0900

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -126,8 +126,11 @@ A
 
 # code
 is_desktop && {
-  wget --trust-server-names -O code_latest.deb 'https://go.microsoft.com/fwlink/?LinkID=760868'
-  sudo apt install ./code_latest.deb -y
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+  sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+  sudo apt update -y
+  sudo apt install code -y
 }
 
 # nanorc
