17e01452417b4653da2431eea9dce4adad35f297
haruna <w10776e8w@yahoo.co.jp>
2026-04-25 02:48:08 +0900

    Change apt install command to use -yU flags

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -79,7 +79,7 @@ else
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
     $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt install -U docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo apt install -yU docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   sudo groupadd docker
   sudo usermod -aG docker $USER
   newgrp docker
